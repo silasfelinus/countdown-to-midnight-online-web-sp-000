55e0295044725204c281ea0d7a1879d5cc24f903
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-08 21:53:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,17 @@
 
 def countdown (number)
   while number > 0
-
-    countdown_with_sleep(number)
+  puts "#{number} SECOND(S)!"
     number -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
+  while number > 0
   puts "#{number} SECOND(S)!"
-  sleep(1)
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
